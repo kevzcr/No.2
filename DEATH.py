e04eb93f8a607794136f281102aee305a9e71c77
kevzcr <88956040+kevzcr@users.noreply.github.com>
2021-10-13 14:24:10 +0000

    Add files via upload

---
+++ DEATH.py
@@ -0,0 +1,7 @@
+a=input("Hello?>>>")
+b=input("where am I?>>>")
+d=input("what is my name?>>>")
+e=input("what is my misson?>>>")
+c=a+" "+b+"."+" "+"My name is"+" "+d+"!"+" "+"My misson is to "+e+"!"
+print(c)
+
